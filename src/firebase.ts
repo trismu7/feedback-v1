97f178f564027d7b9fa74ac2c2e5e01ad759db96
trismu7 <trismu7@gmail.com>
2023-02-02 19:28:46 -0300

    Add get function

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,6 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
-firebase.initializeApp({
-  //infoapiKey: "AIzaSyCTLYL11QQipQvZE8j8R_8P7SyE_gtzBXo",
-});
+firebase.initializeApp({});
 
 export const db = firebase.firestore();
